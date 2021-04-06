d4af888f14a490662a6da15354670e9cc8bee8a2
bidiana <bidianazhazta@gmail.com>
2021-04-06 12:56:38 +0700

    ubah 6 april 2021

--- application/views/template/user/view_status_kasir.php
+++ application/views/template/user/view_status_kasir.php
@@ -21,9 +21,87 @@ foreach ($company_status as $key => $value) {
 
 
 
+<style>
+  input[type=text] {
+    padding: 12px 20px;
+    width: 20em;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 3px solid #ccc;
+    -webkit-transition: 0.5s;
+    transition: 0.5s;
+    outline: none;
+    border-radius: 10px;
+    margin-left: 60px;
+  }
+
+  input[type=text]:focus {
+    border: 2px solid #1E90FF;
+  }
+</style>
 
 
+<!-- CSS untuk style dropdown !-->
+<style>
+  /* Reset Select */
+  select {
 
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0 !important;
+    background: #E8E8E8;
+    background-image: none;
+
+  }
+
+  /* Remove IE arrow */
+  select::-ms-expand {
+    display: none;
+  }
+
+  /* Custom Select */
+  .select {
+    position: relative;
+    display: flex;
+    width: 20em;
+    height: 3em;
+    line-height: 3;
+    background: #F5F5DC;
+    overflow: hidden;
+    border-radius: .25em;
+  }
+
+  select {
+    flex: 1;
+    padding: 0 .5em;
+    color: #000000;
+    cursor: pointer;
+  }
+
+  /* Arrow */
+  .select::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 0 1em;
+    background: #4169E1;
+    cursor: pointer;
+    pointer-events: none;
+    -webkit-transition: .25s all ease;
+    -o-transition: .25s all ease;
+    transition: .25s all ease;
+  }
+
+  /* Transition */
+  .select:hover::after {
+    color: #f39c12;
+  }
+</style>
 
 <main id="main">
 
@@ -62,30 +140,31 @@ foreach ($company_status as $key => $value) {
               <table>
                 <tr>
                   <th>Username</th>
-                  <th><a>: <?= $this->session->userdata('username')  ?></a>
+                  <th><input type="text" disabled name="username" placeholder="username" id="username" minlength="5" maxlength="100" class="input-text" value="<?= $this->session->userdata('username')  ?>">
                   </th>
                 </tr>
 
                 <tr>
                   <th>Pilihan Paket </th>
-                  <th>:
-                    <select required name="m_c_payment_method_id" class='m_c_payment_method_id' id='m_c_payment_method_id'>
-                      <option value="" disabled selected>Pilih Paket</option>
-                      <?php
-                      foreach ($pilihan_payment as $key => $value) {
-                        echo "<option value='" . $value->id . "' label='" . $value->show_text . ' - Rp' . number_format($value->value) . "'></option>";
-                      }
-                      ?>
-                    </select>
+                  <th>
+                    <div class="select" style="margin-left: 60px;">
+                      <select required name="m_c_payment_method_id" class='m_c_payment_method_id' id='m_c_payment_method_id'>
+                        <option value="" disabled selected>Pilih Paket</option>
+                        <?php
+                        foreach ($pilihan_payment as $key => $value) {
+                          echo "<option value='" . $value->id . "' label='" . $value->show_text . ' - Rp' . number_format($value->value) . "'></option>";
+                        }
+                        ?>
+                      </select>
+                    </div>
 
 
                   </th>
                 </tr>
                 <tr>
                   <th>Total Harga</th>
-                  <th>:
+                  <th>
                     <a class="return_data"></a>
-
                   </th>
                 </tr>
 
@@ -234,8 +313,7 @@ foreach ($company_status as $key => $value) {
     font-weight: bold;
   }
 
-  .img-fluid
-  {
+  .img-fluid {
     height: 600px;
   }
 
@@ -248,85 +326,7 @@ foreach ($company_status as $key => $value) {
 
 
 <!------------------------------------------ MENCOBA CUSTOM CONTROL !------------------------------------>
-<style type="text/css">
-  /* class applies to select element itself, not a wrapper element */
-  .select-css {
-    display: block;
-    font-size: 14px;
-    font-family: sans-serif;
-    font-weight: 700;
-    color: #444;
-    line-height: 1.3;
-    padding: .6em 1.4em .5em .8em;
-    width: 100%;
-    max-width: 100%;
-    /* useful when width is set to anything other than 100% */
-    box-sizing: border-box;
-    margin: 0;
-    border: 1px solid #aaa;
-    box-shadow: 0 1px 0 1px rgba(0, 0, 0, .04);
-    border-radius: .5em;
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    appearance: none;
-    background-color: #fff;
-    margin-left: 25px;
-    /* note: bg image below uses 2 urls. The first is an svg data uri for the arrow icon, and the second is the gradient. 
-    for the icon, if you want to change the color, be sure to use `%23` instead of `#`, since it's a url. You can also swap in a different svg icon or an external image reference
-    
-  */
-    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
-      linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
-    background-repeat: no-repeat, repeat;
-    /* arrow icon position (1em from the right, 50% vertical) , then gradient position*/
-    background-position: right .7em top 50%, 0 0;
-    /* icon size, then gradient */
-    background-size: .65em auto, 100%;
-  }
 
-  /* Hide arrow icon in IE browsers */
-  .select-css::-ms-expand {
-    display: none;
-  }
 
-  /* Hover style */
-  .select-css:hover {
-    border-color: #888;
-  }
-
-  /* Focus style */
-  .select-css:focus {
-    border-color: #aaa;
-    /* It'd be nice to use -webkit-focus-ring-color here but it doesn't work on box-shadow */
-    box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
-    box-shadow: 0 0 0 3px -moz-mac-focusring;
-    color: #222;
-    outline: none;
-  }
-
-  /* Set options to normal weight */
-  .select-css option {
-    font-weight: normal;
-  }
-
-  /* Support for rtl text, explicit support for Arabic and Hebrew */
-  *[dir="rtl"] .select-css,
-  :root:lang(ar) .select-css,
-  :root:lang(iw) .select-css {
-    background-position: left .7em top 50%, 0 0;
-    padding: .6em .8em .5em 1.4em;
-  }
-
-  /* Disabled styles */
-  .select-css:disabled,
-  .select-css[aria-disabled=true] {
-    color: graytext;
-    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22graytext%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
-      linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
-  }
-
-  .select-css:disabled:hover,
-  .select-css[aria-disabled=true] {
-    border-color: #aaa;
-  }
-</style>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/jquery.easydropdown.js" type="text/javascript"></script>
